bfa7cffd75a50f20a3f36abc3730b27d72ed3ff9
Abhiman Ranaweera <abhiman.max@gmail.com>
2022-09-01 02:40:02 +0530

    feat: separate the date component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,7 +1,10 @@
+import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css"
 function ExpenseItem ( props) {
+   
 
     return(<div className="expense-item">
+        <ExpenseDate date={props.date}/>
         <div>{props.date.toISOString}</div>
         <div className="expense-item__description">
         <h2>{props.title}</h2>
