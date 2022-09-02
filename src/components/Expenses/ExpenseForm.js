e0f86b20f3774071ed4adcb936ccfc44689f4259
Abhiman Ranaweera <abhiman.max@gmail.com>
2022-09-02 19:31:59 +0530

    feat: multi state handle best practices

--- src/components/Expenses/ExpenseForm.js
+++ src/components/Expenses/ExpenseForm.js
@@ -1,34 +1,99 @@
 import { useState } from "react";
 import "./ExpenseForm.css";
 const ExpenseForm = () => {
-    const [enteredTitle, setEnteredTitle] = useState('');
-    const [enteredAmount, setEnteredAmount] = useState('');
-    const [enteredDate, setEnteredDate] = useState('');
+  // 01 - multiple useStates
+  // const [enteredTitle, setEnteredTitle] = useState('');
+  // const [enteredAmount, setEnteredAmount] = useState('');
+  // const [enteredDate, setEnteredDate] = useState('');
 
-    const titleChangeHandler = (event) => {
-        setEnteredTitle(event.target.value);
-    };
-    const amountChangeHandler = (event) => {
-        setEnteredAmount(event.target.value);
-    };
-    const dateChangeHandler = (event) => {
-        setEnteredDate(event.target.value);
-    };
-console.log(`T ${enteredTitle} A ${enteredAmount} D ${enteredDate}`);
+  const [inputState, setInputState] = useState({
+    enteredTitle: "",
+    enteredAmount: "",
+    enteredDate: "",
+  });
+
+  const titleChangeHandler = (event) => {
+    // 01 - multiple useStates
+    //setEnteredTitle(event.target.value);
+
+    // 02 - single useState but not good always
+    // setInputState({
+    //     ...inputState,
+    //     enteredTitle: event.target.value
+    // });
+
+    //03 - Single useState Best practice
+    setInputState((prvState) => {
+      return {
+        ...prvState,
+        enteredTitle: event.target.value,
+      };
+    });
+  };
+  const amountChangeHandler = (event) => {
+    // 01 - multiple useStates
+    //setEnteredAmount(event.target.value);
+
+    // 02 - single useState but not good always
+    // setInputState({
+    // setInputState({
+    //     ...inputState,
+    //     enteredAmount: event.target.value
+    // });
+
+    //03 - Single useState Best practice
+    setInputState((prvState) => {
+      return {
+        ...prvState,
+        enteredAmount: event.target.value,
+      };
+    });
+  };
+  const dateChangeHandler = (event) => {
+    // 01 - multiple useStates
+    //setEnteredDate(event.target.value);
+
+    // 02 - single useState but not good always
+    // setInputState({
+    //   ...inputState,
+    //   enteredDate: event.target.value,
+    // });
+
+    //03 - Single useState Best practice
+    setInputState((prvState) => {
+      return {
+        ...prvState,
+        enteredDate: event.target.value,
+      };
+    });
+  };
+  console.log(
+    `T ${inputState.enteredTitle} A ${inputState.enteredAmount} D ${inputState.enteredDate}`
+  );
   return (
     <form>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" onChange={ titleChangeHandler }/>
+          <input type="text" onChange={titleChangeHandler} />
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
-          <input type="number" min="0.01" step="0.01" onChange={amountChangeHandler}/>
+          <input
+            type="number"
+            min="0.01"
+            step="0.01"
+            onChange={amountChangeHandler}
+          />
         </div>
         <div className="new-expense__control">
           <label>Date</label>
-          <input type="date" min="2019-01-01" max="2022-12-31" onChange={dateChangeHandler}/>
+          <input
+            type="date"
+            min="2019-01-01"
+            max="2022-12-31"
+            onChange={dateChangeHandler}
+          />
         </div>
       </div>
       <div className="new-expense__actions">
